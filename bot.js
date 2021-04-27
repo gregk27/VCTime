32e7b36a5c43eacf440aa98237eb50b248775975
aree-vanier <git@gregk.ca>
2021-04-26 23:03:12 -0400

    Bot joins channel and updates name with time

--- bot.js
+++ bot.js
@@ -1,10 +1,31 @@
 import { token } from "./secrets.js";
-import Discord from "discord.js";
+import Discord, { DataResolver } from "discord.js";
 
 const client = new Discord.Client();
 
 client.once('ready', ()=>{
     console.log("Ready!");
 })
+
+function getTimeString(){
+    let d = new Date();
+    return `Time: ${d.getHours()}:${d.getMinutes()}`;
+}
+
+client.on('message', message =>{
+    if(message.content.match(/^!time\w*/g)){
+        message.member.voice.channel.join();
+        // Set time on join
+        message.guild.me.setNickname(getTimeString());
+        setTimeout(() => {
+            // Set time before timeout begins
+            message.guild.me.setNickname(getTimeString());
+            setInterval(() => {
+                // Update every minute
+                message.guild.me.setNickname(getTimeString());
+            }, 60000);
+        }, (60-new Date().getSeconds())*1000);
+    }
+});
 
 client.login(token);
